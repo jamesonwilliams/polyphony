f48d47deb3b153c343d3be05ea6af0e2aa04eb72
Jameson Williams <jameson@nosemaj.org>
2023-11-15 12:23:20 -0600

    Workaround Grammarly plugin error message in dev envrionment

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      {/* https://stackoverflow.com/a/76480526/695787 */}
+      <body className={inter.className} suppressHydrationWarning={true}>
+        {children}
+      </body>
     </html>
   );
 }
