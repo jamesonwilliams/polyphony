7d8b760dc18f1f74c3f4175ff2e378e574e64f47
Jameson Williams <jameson@nosemaj.org>
2023-11-15 02:34:53 -0600

    Wrap PolySynth into useRef() so its not a global anymore

--- app/TonePlayer.tsx
+++ app/TonePlayer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { PolySynth } from 'tone';
 
 interface TonePlayerProps {
@@ -6,27 +6,38 @@ interface TonePlayerProps {
   muted: boolean;
 }
 
-const synth = new PolySynth().toDestination();
-
 const TonePlayer: React.FC<TonePlayerProps> = ({ tones, muted }) => {
   const [lastPlaying, setLastPlaying] = useState<string[]>([]);
+  const synthRef = useRef<PolySynth | null>(null);
 
-  const stopTones = (toStop: string[]) => {
-    toStop.forEach((tone) => synth.triggerRelease(tone));
-  };
-
-  const startTones = (toStart: string[]) => {
-    toStart.forEach((tone) => synth.triggerAttack(tone));
-  };
-
+  // Once on component mount
   useEffect(() => {
+    synthRef.current = new PolySynth().toDestination();
+    return () => {
+      synthRef.current?.dispose();
+      synthRef.current = null;
+    };
+  }, []);
+
+  // Whenever the props/state change
+  useEffect(() => {
+    const stopTones = () => {
+      const removedTones = lastPlaying.filter((tone) => !tones.includes(tone));
+      removedTones.forEach((tone) => synthRef.current?.triggerRelease(tone));
+    };
+
+    const startTones = () => {
+      const addedTones = tones.filter((tone) => !lastPlaying.includes(tone));
+      addedTones.forEach((tone) => synthRef.current?.triggerAttack(tone));
+    };
+
+    const synth = synthRef.current;
+    if (synth === null) return;
+
     synth.context.resume().then(() => {
-      stopTones(lastPlaying.filter((tone) => !tones.includes(tone)));
-      startTones(tones.filter((tone) => !lastPlaying.includes(tone)));
-      // The intersection of next and previous keeps playing
-
+      stopTones();
+      startTones();
       setLastPlaying(tones);
-
       synth.volume.value = muted ? -Infinity : 0 /* db */;
     });
   }, [tones, muted, lastPlaying]);
